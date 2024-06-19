dfbf999b210a68ee8abffb444096a5066e9aaad0
Kamiyo <kamiyo.k91@gmail.com>
2024-06-19 10:28:35 +0200

    Hotfix Combat Read. Buff

--- Skill/S_Yae_21341_7.cs
+++ Skill/S_Yae_21341_7.cs
@@ -6,18 +6,27 @@ namespace YaeMod21341
     /// <summary>
     ///     Combat Readiness
     /// </summary>
-    public class S_Yae_21341_7 : Skill_Extended, IP_SkillUse_Target
+    public class S_Yae_21341_7 : Skill_Extended, IP_SkillUse_Target, IP_BuffAddAfter
     {
         private static readonly List<string> SkillKeys = new List<string>
             { "S_Yae_21341_0", "S_Yae_21341_1", "S_Yae_21341_2", "S_Yae_21341_5" };
 
+        public override void Init()
+        {
+            OnePassive = true;
+        }
+
         public void AttackEffect(BattleChar hit, SkillParticle SP, int DMG, bool Cri)
         {
             if (SP.SkillData != MySkill) return;
-            KamiyoUtil.GetBuff<B_YaeCombatRead_21341>(BChar, nameof(B_YaeCombatRead_21341))?.CustomInit();
             if (!Cri) return;
             KamiyoUtil.DrawCharacterSkill(BChar,
                 KamiyoUtil.PrepareRandomSkill(BChar, SkillKeys, new KamiyoSkillChangeParameters()));
+        }
+
+        public void BuffaddedAfter(BattleChar BuffUser, BattleChar BuffTaker, Buff addedbuff, StackBuff stackBuff)
+        {
+            KamiyoUtil.GetBuff<B_YaeCombatRead_21341>(BChar, nameof(B_YaeCombatRead_21341), false)?.CustomInit();
         }
     }
 }
